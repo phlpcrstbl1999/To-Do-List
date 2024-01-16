20ca50cedb037067dc42acb493d8315b3bb2b042
Philip Cristobal <philipcristobal06@gmail.com>
2024-01-16 14:28:30 +0800

    Working with add/remove to do list using useState

--- src/components/ToDoListComponent.js
+++ src/components/ToDoListComponent.js
@@ -3,24 +3,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './ToDoListComponent.css';
 
-const todolist = ['Learn Javascript', 'Learn React', 'Build a React App'];
-
+const toDolist = ['Learn Javascript', 'Learn React', 'Build a React App'];
 const ToDoListComponent = () => {
-    const[lists, setLists] = useState(todolist);
+    const[lists, setLists] = useState(toDolist);
+    const[whatToDo, setWhatToDo] = useState("");
+    const[itemsLeft, setItemsLeft] = useState(toDolist.length);
+    const handleWhatToDo = (e) => {
+        setWhatToDo(e.target.value);
+    }
+
+    const addList = () => {
+        const addItemsLeft = itemsLeft + 1;
+        setLists([...lists, whatToDo]);
+        setWhatToDo('');
+        setItemsLeft(addItemsLeft);
+        alert("Added Successfuly")
+        
+    }
+    const handleAddWhatToDo = () => {
+        whatToDo === "" ? alert("Please enter what to do") : addList();
+    }
+    const handleCheckBox = (e) => {
+        const index = e.target.value;
+        const updatedList = [...lists];
+        const minusItemsLeft = itemsLeft - 1;
+        updatedList.splice(index, 1);
+        setLists(updatedList);
+        setItemsLeft(minusItemsLeft);
+    }
     return <div className="container">
             {/* <h1>THINGS TO DO</h1> */}
-            <input type="text" className="whatToDo" placeholder='Add New'></input>
-            {lists.map((list, index) => <>
-                <label className="list-container">
-                        <input type="checkBox" value={index}></input>
-                        <li key={index}>{list}</li>
+            <input type="text" className="whatToDo" placeholder='Add New' onChange={handleWhatToDo} id="whatToDo" value={whatToDo}></input>
+            {lists.map((list, index) => (
+                <label className="list-container" key={index} value={index}>
+                        <input type="checkBox" value={index} onChange={handleCheckBox} checked={false}></input>
+                        <li>{list}</li>
                 </label>
-            </>)}
+            ))}
             <div className="btn-container">
-                <button class="plus-btn"><FontAwesomeIcon icon={faPlus} /></button>
-                <p>{todolist.length} items left</p>
+                <button className="plus-btn" onClick={handleAddWhatToDo}><FontAwesomeIcon icon={faPlus} /></button>
+                <p>{itemsLeft} items left</p>
             </div>
-            
         </div>
 }
 
