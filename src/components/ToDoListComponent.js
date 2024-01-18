099e9b71c8738fbe7bd18d8a4e00f399ee9c1e95
Philip Cristobal <philipcristobal06@gmail.com>
2024-01-18 09:57:11 +0800

    Fixing checkbox and other functions

--- src/components/ToDoListComponent.js
+++ src/components/ToDoListComponent.js
@@ -1,26 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import './ToDoListComponent.css';
 
 const toDolist = [
-    { todo: 'Learn Javascript', status: 'incomplete' },
-    { todo: 'Learn React', status: 'incomplete' },
-    { todo: 'Build a React App', status: 'incomplete' }
+    { id: 0, todo: 'Learn Javascript', status: 'incomplete' },
+    { id: 1, todo: 'Learn React', status: 'incomplete' },
+    { id: 2, todo: 'Build a React App', status: 'incomplete' }
   ];
 const ToDoListComponent = () => {
     const[lists, setLists] = useState(toDolist);
     const[whatToDo, setWhatToDo] = useState("");
-    const[itemsLeft, setItemsLeft] = useState(toDolist.filter(list => list.status === 'incomplete').length);
+    const[itemsLeft, setItemsLeft] = useState(lists.filter(list => list.status === 'incomplete').length);
     const[isActive, setIsActive] = useState(true);
     const[sortLists, setSortLists] = useState(lists);
-    const completedList = [...lists];
     const handleWhatToDo = (e) => {
         setWhatToDo(e.target.value);
     }
     const addList = () => {
         const addItemsLeft = itemsLeft + 1;
-        const newToDo = {todo: whatToDo, status: 'incomplete'};
+        const listsCount = lists.length + 1;
+        const newToDo = {id: listsCount - 1, todo: whatToDo, status: 'incomplete'};
         setLists([...lists, newToDo]);
         setSortLists([...lists, newToDo]);
         setWhatToDo('');
@@ -31,22 +31,21 @@ const ToDoListComponent = () => {
         whatToDo === "" ? alert("Please enter what to do") : addList();
     }
     const handleCheckBox = (e) => {
-        const index = e.target.value;
-       
-        let setStatus = completedList[index];
-        setStatus.status === 'incomplete' ? setStatus.status = 'completed' : setStatus.status = 'incomplete';
-        const listCount = completedList.filter(list => list.status === 'incomplete').length;
+        const id = e.target.value;
+        let getList = lists[id];
+        getList.status === 'incomplete' ? getList.status = 'completed' : getList.status = 'incomplete';
+        const listCount = lists.filter(list => list.status === 'incomplete').length;
         setItemsLeft(listCount);
     }
     const handleIsActive = () => {
-        const completedCount = completedList.filter(list => list.status === 'completed').length;
-        const listCount = completedList.filter(list => list.status === 'incomplete').length;
-        const completedHandler = completedList.filter(list => list.status === 'completed');
+        const completedCount = lists.filter(list => list.status === 'completed').length;
+        const listCount = lists.filter(list => list.status === 'incomplete').length;
+        const completedHandler = lists.filter(list => list.status === 'completed');
         if(isActive) {
             setSortLists(completedHandler);
             setItemsLeft(completedCount);
         }else {
-            setSortLists(completedList);
+            setSortLists(lists);
             setItemsLeft(listCount);
         }
         setIsActive(!isActive);
@@ -54,9 +53,9 @@ const ToDoListComponent = () => {
     return <div className="container">
             <h1 className="title">THINGS TO DO</h1>
             <input type="text" className="whatToDo" placeholder='Add New' onChange={handleWhatToDo} id="whatToDo" value={whatToDo}></input>
-            {sortLists.map((list, index) => (
-                <label className="list-container" key={index} value={index}>
-                        <input type="checkBox" value={index} onChange={handleCheckBox} id={index}></input>
+            {sortLists.map((list) => (
+                <label className="list-container" key={list.id} value={list.id}>
+                        <input type="checkBox" value={list.id} onChange={handleCheckBox} id={list.id} disabled={isActive ? '' : 'disabled'}></input>
                         <li className={`${list.status === 'completed' ? 'completed' : ''}`}>{
                             list.todo
                         }</li>
@@ -67,7 +66,7 @@ const ToDoListComponent = () => {
 
             <div className="footer">
                 <div className="btn-container">
-                    <button className="plus-btn" onClick={handleAddWhatToDo}><FontAwesomeIcon icon={faPlus} /></button>
+                    <button className="plus-btn" onClick={handleAddWhatToDo} disabled={isActive ? '' : 'disabled'}><FontAwesomeIcon icon={faPlus} /></button>
                     <p>{itemsLeft} items left</p>
                 </div>
                 <div>
